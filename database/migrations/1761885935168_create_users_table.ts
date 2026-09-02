176c7f0328324d0cbc6a5671c7d48515a302fa76
Hugo <hugobatista2710@gmail.com>
2026-09-02 20:44:35 -0300

    feat: cria migrations e models para user_establishment e user_customer

--- database/migrations/1761885935168_create_users_table.ts
+++ database/migrations/1761885935168_create_users_table.ts
@@ -6,9 +6,11 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
-      table.string('full_name').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
+      table.string('user_type', 30).notNullable().defaultTo('CUSTOMER')
+      table.string('status', 20).notNullable().defaultTo('ACTIVE')
+      table.timestamp('last_login_at').nullable()
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
